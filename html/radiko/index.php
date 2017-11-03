5d0b09859c853adf87c484568d4cc61734d6f961
Hide <takewick@gmail.com>
2017-11-03 21:57:26 +0900

    initial commit

---
+++ html/radiko/index.php
@@ -0,0 +1,20 @@
+<?php $title = 'Radiko' ?>
+<?php $path = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/' ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<rss version="2.0">
+  <channel>
+    <title><?php echo $title ?></title>
+    <description><?php echo $title ?> Podcast</description>
+    <link><?php echo $path ?></link>
+    <language>ja</language>
+<?php foreach (glob('*.m4a') as $file): ?>
+<?php   preg_match('/(\d{4})-(\d{2})-(\d{2})/', $file, $matches) ?>
+    <item>
+      <title><?php echo $matches[0] ?></title>
+      <description><?php echo $matches[0] ?></description>
+      <pubDate><?php echo date('r', mktime($matches[4], $matches[5], 0, $matches[2], $matches[3], $matches[1])) ?></pubDate>
+      <enclosure url="<?php echo $path ?><?php echo $file ?>" length="<?php echo filesize($file) ?>" type="<?php echo mime_content_type($file) ?>" />
+    </item>
+<?php endforeach ?>
+  </channel>
+</rss>
